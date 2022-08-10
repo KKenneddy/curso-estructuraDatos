c52043914c204ecd92bd21b87a13d3d4ddcf9098
Kennedy <nofuequequise@gmail.com>
2022-08-10 16:24:13 -0400

    Cap14

--- src/linkedList/singly.js
+++ src/linkedList/singly.js
@@ -49,6 +49,30 @@ class MySinglyLinkedList {
         this.length++;
         return this;
     }
+    insert(index, value){
+        if(index >= this.length){
+            return this.append(value);
+        }
+
+        const newNode = new Node(value);
+        const firstPointer = this.getTheIndex(index -1);
+        const holdingPointer = firstPointer.next;
+        firstPointer.next = newNode;
+        newNode.next = holdingPointer;
+
+        this.length++;
+        return this;
+    }
+    getTheIndex(index){
+        let counter = 0;
+        let currentNode = this.head;
+
+        while(counter !== index){
+            currentNode = currentNode.next
+            counter++;
+        }
+        return currentNode;
+    }
 }
 
 let mySinglyLinkedList = new MySinglyLinkedList(1);
